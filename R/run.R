f02656a356bab9601f185866f52608af2c562efa
Ethan White <ethan@weecology.org>
2020-03-08 20:42:25 -0400

    Add function to run full update
    Fix and clean up other aspects of code

---
+++ R/run.R
@@ -0,0 +1,12 @@
+update <- function() {
+    current_data <- load_current_data()
+    current_date_time <- tail(current_data, 1)$date_time
+    webpage <- get_html()
+    scraped_date_time <- scrape_update_date_time(webpage)
+    new_date_time <- process_date(scraped_date_time)
+    if (new_date_time != current_date_time){
+      scraped_data <- scrape_data(webpage)
+      updated_data <- process_data(scraped_data, new_date_time)
+      write_data(updated_data)
+    }
+}
